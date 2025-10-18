7ea4a0087ce8cec533a42f9f3372f3e6f1e8a164
unknownsaying <15368104568@189.cn>
2025-10-18 09:08:53 +0800

    Update Algebra1.c

--- calculate/Algebra1.c
+++ calculate/Algebra1.c
@@ -8,3 +8,12 @@
 #define eight I
 #define nine O
 #define ten P
+#define eleven A
+#define twelve S
+#define thirteen D
+#define fourteen F
+#define fifteen G
+#define sixteen H
+#define seventeen J
+#define eighteen K
+#define nineteen L
